085cce93537099223ac2922e7da2cd298e906b8c
khamidullox <hamidullo22022@gmail.com>
2024-02-19 16:10:21 +0500

    js methods

---
+++ app.js
@@ -0,0 +1,59 @@
+let ism  = 'Jone Doge'
+let result = ism.includes('l')
+console.log(result)
+
+
+let yosh = 54
+console.log(yosh == 54)
+console.log(yosh != 54)
+console.log(yosh > 20)
+console.log(yosh < 30)
+
+
+console.log(ism == "Jone Doge")
+
+let natija = "100"
+
+console.log(+natija + 1)
+
+
+let resultString = String(15)
+console.log(resultString)
+
+let email = 'hamidullo22022@gmail.com'
+
+let emailUpCase = email.toUpperCase()
+console.log(emailUpCase)
+
+
+let emailLowCase = email.toLowerCase()
+console.log(emailLowCase)
+
+
+let emailIndex = email.indexOf('l')
+console.log(emailIndex)
+
+
+let emailLastIndex = email.lastIndexOf('l')
+console.log(emailLastIndex)
+
+
+let emailSlice = email.slice( 2, 8 )
+console.log(emailSlice)
+
+
+let emailSubstr = email.substr(4 , 10)
+console.log(emailSubstr)
+
+
+let emailReplace = email.replace('l' ,'ha')
+console.log(emailReplace)
+
+
+let emailAt = email.charAt()
+console.log(emailAt)
+
+let emailTrim = email.trim()
+
+console.log(emailTrim)
+
